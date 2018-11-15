ca88e23a9a0aed9e9383d904106bb8485db56dfc
Faramanta <katuxa@yandex.ru>
2018-11-15 10:38:57 +0500

    Updates

---
+++ precache-manifest.82a71246981ea02e7aec56e33e348c84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "80f8d96c4f949a3e8cdba9cd045199eb",
+    "url": "/cats-react/static/media/cat.80f8d96c.png"
+  },
+  {
+    "revision": "9c94c76cf9548b5d26c56dcc183a373d",
+    "url": "/cats-react/static/media/bg.9c94c76c.jpg"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/cats-react/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "d16fb3302e926c2f7a15",
+    "url": "/cats-react/static/js/runtime~main.d16fb330.js"
+  },
+  {
+    "revision": "3f46709b7246985d63d3",
+    "url": "/cats-react/static/js/main.3f46709b.chunk.js"
+  },
+  {
+    "revision": "00999e02dd68d63dc740",
+    "url": "/cats-react/static/js/1.00999e02.chunk.js"
+  },
+  {
+    "revision": "3f46709b7246985d63d3",
+    "url": "/cats-react/static/css/main.ec58c964.chunk.css"
+  },
+  {
+    "revision": "51005cb420ee11c1ce635132200ab9f3",
+    "url": "/cats-react/index.html"
+  }
+];
