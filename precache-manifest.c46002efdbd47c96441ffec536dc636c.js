8056e8c4f8838db6e7e6666f6a03595e22c1de9d
Faramanta <katuxa@yandex.ru>
2018-11-15 11:39:46 +0500

    Updates

---
+++ precache-manifest.c46002efdbd47c96441ffec536dc636c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "80f8d96c4f949a3e8cdba9cd045199eb",
+    "url": "./static/media/cat.80f8d96c.png"
+  },
+  {
+    "revision": "9c94c76cf9548b5d26c56dcc183a373d",
+    "url": "./static/media/bg.9c94c76c.jpg"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "./static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9a7b8807eccc830b424e",
+    "url": "./static/js/main.9a7b8807.chunk.js"
+  },
+  {
+    "revision": "00999e02dd68d63dc740",
+    "url": "./static/js/1.00999e02.chunk.js"
+  },
+  {
+    "revision": "9a7b8807eccc830b424e",
+    "url": "./static/css/main.1c146b20.chunk.css"
+  },
+  {
+    "revision": "3a0b31c5365aa6ad03717ce87520d529",
+    "url": "./index.html"
+  }
+];
